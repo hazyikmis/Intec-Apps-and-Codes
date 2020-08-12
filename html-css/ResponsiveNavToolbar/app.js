dfc630abe7780f1fc9f9a8085656538d677520d7
Halil Azyikmis <37673859+hazyikmis@users.noreply.github.com>
2020-08-12 03:44:33 +0200

    sidebar nav menu fixed

--- html-css/ResponsiveNavToolbar/app.js
+++ html-css/ResponsiveNavToolbar/app.js
@@ -2,7 +2,35 @@ const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-links");
   burger.addEventListener("click", () => {
+    //toggle nav
     nav.classList.toggle("nav-active");
+    //animate links
+    const navLinks = document.querySelectorAll(".nav-links li");
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 5 + 0.5
+        }s`; //last part is delay
+        // console.log(index / 7);
+        //"forwards" is IMPORTANT to retain the last style from animation
+      }
+    });
+    //burger animation
+    burger.classList.toggle("toggle");
+  });
+
+  //when window expanded while the menu still open, this code below closes the menu
+  window.addEventListener("resize", () => {
+    // console.log(this.innerWidth, this.innerHeight);
+    if (this.innerWidth > 768) {
+      const nav = document.querySelector(".nav-links");
+      if (nav.classList.contains("nav-active")) {
+        const burger = document.querySelector(".burger");
+        burger.click();
+      }
+    }
   });
 };
 
